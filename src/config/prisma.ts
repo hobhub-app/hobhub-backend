5d012bdea1abbc97324c480a79a09ff634115f71
Sarah Österhed <sarah.osterhed@gmail.com>
2025-12-11 15:31:23 +0100

    chore(deplyment): updated prisma import

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../generated/prisma/index.js";
 
 // Create adapter with connection string
 const adapter = new PrismaPg({
