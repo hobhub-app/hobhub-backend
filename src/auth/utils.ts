09b46fc7d92795d42b0e695db960affa12479ece
Sarah Österhed <sarah.osterhed@gmail.com>
2025-12-11 16:44:13 +0100

    feat(auth): implement utilities for hashing and verifying password

--- src/auth/utils.ts
+++ src/auth/utils.ts
@@ -1,1 +1,14 @@
-// Password hashing, token generation
+import bcrypt from "bcryptjs";
+
+export const hashPassword = async (userPassword: string): Promise<string> => {
+  const hashedPassword = await bcrypt.hash(userPassword, 10);
+  return hashedPassword;
+};
+
+export const verifyPassword = async (
+  userPassword: string,
+  hashedPassword: string
+): Promise<boolean> => {
+  const isMatch = await bcrypt.compare(userPassword, hashedPassword);
+  return isMatch;
+};
