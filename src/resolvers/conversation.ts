b1e78b78d40ac6ee3025b559fbad45ba15f36735
Sarah Österhed <sarah.osterhed@gmail.com>
2025-12-21 12:42:54 +0100

    chore: add missing import extentions

--- src/resolvers/conversation.ts
+++ src/resolvers/conversation.ts
@@ -1,5 +1,5 @@
 import { AuthContext } from "../auth/types";
-import { prisma } from "../config/prisma";
+import { prisma } from "../config/prisma.js";
 import {
   createMessage,
   getMessagesByConversation,
