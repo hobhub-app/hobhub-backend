7615cc6f79c12f71eb94997b06d8b1fa46ae2e07
Sarah Österhed <sarah.osterhed@gmail.com>
2025-12-21 10:24:28 +0100

    feat(chat): add resolvers for conversations

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,15 +1,18 @@
 import { userResolvers } from "./user.js";
 import { hobbyResolvers } from "./hobby.js";
 import { authResolvers } from "./auth.js";
+import { conversationResolvers } from "./conversation.js";
 
 export const resolvers = {
   Query: {
     ...userResolvers.Query,
     ...hobbyResolvers.Query,
+    ...conversationResolvers.Query,
   },
   Mutation: {
     // ...userResolvers.Mutation,
     ...hobbyResolvers.Mutation,
     ...authResolvers.Mutation,
+    ...conversationResolvers.Mutation,
   },
 };
